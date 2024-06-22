3ae7fadff0a2ecea78abb459050a82fa53ef0cf6
subham sarangi <subhamsarangi23@gmail.com>
2024-06-22 23:40:13 +0530

    Mount the userRouter on specified path

--- src/app.js
+++ src/app.js
@@ -2,14 +2,25 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-
 const app = express();
 
-app.use(cors({
-    origin:process.env.ORIGIN,
-    credentials:true
-}))
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true ,limit:"16kb" }))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "50kb" }));
+app.use(express.urlencoded({ extended: true, limit: "50kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+// Routes imports
+
+import userRouter from "./routes/user.routes.js";
+
+//Routes declaration
+
+app.use("/api/v1/users", userRouter);
+
+export { app };
