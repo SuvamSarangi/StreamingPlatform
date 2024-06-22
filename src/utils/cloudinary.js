0fd26f570363f324e662dc2df9337102c7407357
subham sarangi <subhamsarangi23@gmail.com>
2024-06-22 23:47:33 +0530

    Integrated dotenv for environment variable management.

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,29 +1,34 @@
+import dotenv from "dotenv";
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
-
+dotenv.config({
+  path: "./.env",
+});
 cloudinary.config({
-    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-    api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARY_API_SECRET
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-const uploadOnCloudinary = async (localFilePath)=>{
-    try {
-        if(!localFilePath) return null;
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
 
-        //Upload the file on cloudinary
-        const response = await cloudinary.uploader.upload(localFilePath,{
-            resource_type:"auto"
-        })
-        // file has been uploaded successfully
-        console.log("uploaded successfully",response.url)
-        return response;
-    } catch (error) {
-        fs.unlinkSync(localFilePath)  // remove the locally saved temporary file as the upload operation got failed
+    //Upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
 
-        return null;
-    }
+    // file has been uploaded successfully
+    // console.log("uploaded successfully", response.url);
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    // console.error("Cloudinary upload error:", error);
+    fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
 
-}
+    return null;
+  }
+};
 
-export {uploadOnCloudinary}
+export { uploadOnCloudinary };
